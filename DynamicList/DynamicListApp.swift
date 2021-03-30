b246cfcc7937ed09d624b2d5ad6f1cb706e6b901
Leandro Vitor <vitoleandro@gmail.com>
2021-03-30 20:40:48 -0300

    Initial Commit

---
+++ DynamicList/DynamicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicListApp.swift
+//  DynamicList
+//
+//  Created by Leandro Vitor on 30/03/21.
+//
+
+import SwiftUI
+
+@main
+struct DynamicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
